747a53eb3e123d78d17a4d5217786eb94101441b
tunnckoCore <mameto_100@mail.bg>
2017-01-03 01:32:50 +0200

    fix(*): cleanup

--- src/test.js
+++ src/test.js
@@ -2,10 +2,13 @@ import test from 'mukla'
 import rolldown from './index'
 
 test('rolldown', (done) => {
+  let called = 0
   rolldown({
     source: `export default (foo) => Promise.resolve(foo)`,
     ongenerate: (opts) => {
-      // console.log('on generate')
+      test.strictEqual(typeof opts, 'object')
+      test.strictEqual(typeof opts.bundle, 'object')
+      called++
     },
     format: 'cjs',
     plugins: [['buble', { target: { node: '0.12' } }]]
@@ -13,6 +16,7 @@ test('rolldown', (done) => {
     test.strictEqual(/function/.test(result.code), true)
     test.strictEqual(/module/.test(result.code), true)
     test.strictEqual(/exports/.test(result.code), true)
+    test.strictEqual(called, 1)
     done()
   }, done).catch(done)
 })
